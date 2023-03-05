479132dfbc30e7743d318476a0d06d7e7d54c958
SpSrividya <36660964+SpSrividya@users.noreply.github.com>
2023-03-05 16:55:32 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
-import Converter from "./components/organisms/Converter/Converter";
+// import Converter from "./components/organisms/Converter/Converter";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import About from "./Routes/About";
 
 function App() {
   return (
     <div className="App">
-      <header></header>
-      <body>
-        <div className="title">Chakrie</div>
-        {/* <Converter /> */}
-      </body>
-      <footer></footer>
+      {/* <header className="App-header">
+        <h1 className="App-title">Welcome to My App</h1>
+      </header> */}
+      <div className="menu">
+        <ul>
+          <li>
+            {" "}
+            <Link to="/">Home</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to="/about">About</Link>{" "}
+          </li>
+        </ul>
+      </div>
+      <div className="App-intro">
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./Routes/Root";
+import ErrorPage from './Routes/ErrorPage';
+import Contact from './Routes/Contact';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
